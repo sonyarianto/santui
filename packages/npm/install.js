52e776d9816e95667ab2eff83a1c4dd122726ed9
Sony AK <sony@sony-ak.com>
2026-07-05 12:14:05 +0700

    chore: bump to v0.1.3

--- packages/npm/install.js
+++ packages/npm/install.js
@@ -4,7 +4,7 @@ const path = require("path");
 const https = require("https");
 const { execSync } = require("child_process");
 
-const VERSION = "0.1.2";
+const VERSION = "0.1.3";
 const BIN_DIR = path.join(__dirname, "bin");
 
 function getBinaryName() {
